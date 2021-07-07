23f221b5daae451925f880447b9da7d3e41a1d84
tradalhariri <trad.alhariri@gmail.com>
2021-07-07 23:17:31 +0300

    add todos

--- js/cart.js
+++ js/cart.js
@@ -19,11 +19,32 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let tbody = document.getElementsByTagName('tbody')[0];
+  tbody.textContent = '';
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
+  let tbody = document.getElementsByTagName('tbody')[0];
+
+  
+  for(let i= 0;i<cart.items.length;i++){
+    let trEl = document.createElement('tr');
+    let tdEl1 = document.createElement('td');
+    tdEl1.textContent = '✂';
+    let tdEl2 = document.createElement('td');
+    tdEl2.textContent = cart.items[i].quantity;
+    
+    let tdEl3 = document.createElement('td');
+    tdEl3.textContent = cart.items[i].product;
+    trEl.appendChild(tdEl1);
+    trEl.appendChild(tdEl2);
+    trEl.appendChild(tdEl3);
+    tbody.appendChild(trEl);
+  }
   // TODO: Find the table body
 
   // TODO: Iterate over the items in the cart
@@ -34,6 +55,16 @@ function showCart() {
 }
 
 function removeItemFromCart(event) {
+ let tr = event.target.parentNode;
+console.log(tr);
+tr.classList.add('remove');
+let item = document.querySelector(`.remove td:nth-child(3)`).innerHTML;
+console.log(cart);
+cart.removeItem(item);
+console.log(cart);
+table.deleteRow(tr.rowIndex);
+cart.saveToLocalStorage();
+renderCart();
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
