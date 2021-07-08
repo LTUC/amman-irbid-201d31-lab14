c8f56e8b4aed33239c27fb56935d2233ffaa8571
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-07-08 11:14:50 +0300

    lab14-final

--- js/cart.js
+++ js/cart.js
@@ -70,9 +70,10 @@ function removeItemFromCart(event) {
 cart.removeItem(event.target.id) ;
 cart.saveToLocalStorage();
 renderCart();
+
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
-  // TODO: Re-draw the cart table
+  // TODO: Re-draw the cart table.
 
 }
 
